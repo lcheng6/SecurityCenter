2c3137ca789f64c15a0c6379b70e98df8cb07eb5
SAPGSS\C5254476 <liang.cheng@reancloud.com>
2017-01-13 14:12:46 -0500

    Update SC5API

--- SC5API.py
+++ SC5API.py
@@ -14,8 +14,8 @@ class SecurityCenterAPI:
     def __init__(self): 
         self.data = {}
     
-	def set_url(self, url):
-		self.url = url
+    def set_url(self, url):
+        self.url = url
 		
     def build_url(self, restCall):
         """ Formats the SC URL with the rest API call"""
