872d5c348dd89851d28d87fe2394c89c5ee293e0
SAPGSS\C5254476 <liang.cheng@reancloud.com>
2017-01-16 12:02:24 -0500

    change test code

--- AWS_Inv_test.py
+++ AWS_Inv_test.py
@@ -4,7 +4,7 @@ import json
 
 aws_api = AWSInv.AWSInventory();
 
-aws_api.set_aws_region('us-west-gov-1')
+aws_api.set_aws_region('us-gov-west-1')
 aws_api.aws_ec2_inventory()
 
 aws_linux_hosts = aws_api.get_linux_hosts()
