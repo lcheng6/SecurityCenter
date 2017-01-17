5b40d32ba116019afcc596f90600d575f59ebf5a
SAPGSS\C5254476 <liang.cheng@reancloud.com>
2017-01-17 17:25:25 -0500

    fix typo

--- SC5API.py
+++ SC5API.py
@@ -103,7 +103,7 @@ class SecurityCenterAPI:
     
     def post_hosts_to_asset(self, id, hosts_ips):
         #Post the hosts private IPs to the asset identified by ID
-        patch_records = {typeFields: {definedIPs:'10.195.16.1, 10.195.16.2, 10.195.16.4, 10.195.16.5'}};
+        patch_records = {'typeFields': {'definedIPs' :'10.195.16.1, 10.195.16.2, 10.195.16.4, 10.195.16.5'}};
         
         data = self.connect('PATCH', '/rest/asset/{IP}'.format(id), patch_records)
         results = data.json()['response'];
