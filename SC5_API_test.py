5d8abda6ea8078cd64dd0fd02e5bc7d891a54c2a
SAPGSS\C5254476 <liang.cheng@reancloud.com>
2017-01-17 11:08:56 -0500

    update SC5_API_test with Argument parser

--- SC5_API_test.py
+++ SC5_API_test.py
@@ -1,16 +1,22 @@
 import SC5API
 import sys
 import json
+import argpase improt ArugmentParser
 
-api = SC5API.SecurityCenterAPI()
-api.set_url('https://10.14.226.13')
-api.login(sys.argv[1], sys.argv[2])
+parser = argparse.ArgumentParser(description='Enter your Nessus Security Center host name, uname, and password')
+
+parser.add_argument('-h', dest = 'hostname', type=str, required=True)
+parser.add_argument('-u', dest = 'user', type=str, required=True)
+parser.add_argument('-p', dest = 'password', type=str, required=True)
 
 
-#assetList = api.get_assets()
-#for id in assetList:
-#    print("Asset ID: "+id)
-    
-asset_219 = api.get_asset_by_id(219);
+args = parser.parse_args()
+if args.user
+    print args.user;
+#api = SC5API.SecurityCenterAPI()
+#api.set_url('https://10.14.226.13')
+#api.login(sys.argv[1], sys.argv[2])
 
-json.dumps(asset_219);
+#asset_219 = api.get_asset_by_id(219);
+
+#json.dumps(asset_219);
