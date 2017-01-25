62e0e3bca65a7b3b86552eb57a30fb66fba6bab9
SAPGSS\C5254476 <liang.cheng@reancloud.com>
2017-01-25 10:45:22 -0500

    SC5_API_Test update.

--- SC5_API_test.py
+++ SC5_API_test.py
@@ -7,9 +7,10 @@ import pprint
 parser = argparse.ArgumentParser(description='Enter your Nessus Security Center host name, uname, and password')
 pp = pprint.PrettyPrinter(indent=4);
 
-parser.add_argument('--hostname', dest = 'hostname', type=str, required=True, help='hostname of the Nessus Security Center')
+parser.add_argument('--schost', dest = 'hostname', type=str, required=True, help='hostname or IP Address of the Nessus Security Center')
 parser.add_argument('-u', dest = 'user', type=str, required=True, help='Nessus Security Center username')
 parser.add_argument('-p', dest = 'password', type=str, required=True, help='Password')
+#parser.add_argument('-c', dest = 'config', type =str, required=True, help='Configuration File')
 
 
 args = parser.parse_args()
@@ -19,7 +20,7 @@ url = 'https://' + args.hostname
 api.set_url(url)
 api.login(args.user, args.password)
 
-api.update_hosts_by_asset_id(219, '10.191.1.1, 10.191.1.2, 10.191.1.3, 10.191.1.4, 10.191.1.5, 10.191.1.10');
+api.update_hosts_by_asset_id(219, '10.191.1.1, 10.191.1.2, 10.191.1.3, 10.191.1.4, 10.191.1.5, 10.191.1.10/32');
 
 asset_219 = api.get_asset_by_id(219);
 
