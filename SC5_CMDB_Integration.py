ae7cbc3067307eb0b621ac6134f0b12fc78f9925
SAPGSS\C5254476 <liang.cheng@reancloud.com>
2017-02-13 15:06:26 -0500

    additional comments

--- SC5_CMDB_Integration.py
+++ SC5_CMDB_Integration.py
@@ -113,8 +113,10 @@ logging.info(args.user + ',' + 'successfully logged to Nessus Scanner ' + securi
 
 #save a back up of assets from the security center
 
+#Get the linux asset from Security Center API
 #TODO: log the content of linux asset
 linux_asset = securityCenterAPI.get_asset_by_id(securityCenterInitData["linuxAssetId"])
+#Get the linux asset from Security Center API
 #TODO: log the content of windows asset
 windows_asset = securityCenterAPI.get_asset_by_id(securityCenterInitData["windowsAssetId"])
 
